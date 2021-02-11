2be50e54c9aa48fa433a3c25c7b02fab0b758491
Maytee Srisomdee <matee@kkumail.com>
2021-02-11 15:41:35 +0700

    upload

--- Week5.2.2.py
+++ Week5.2.2.py
@@ -6,7 +6,6 @@ print('2.เพิ่มรายการสินค้ากด b')
 print('3.ออกจากระบบกด c')
 a = input('การุณาเลือกทำรายการ :')
 print('-'*30)    
-
 class shop :
     def __init__ (self ,namestore ,goods ) :
         self.namestore = namestore
@@ -14,22 +13,16 @@ class shop :
     def showfood(self) :
         print('ชื่อร้าน :',self.namestore)
         print('รายการอาหาร :\n',self.goods)
-class goodsnew :
-    def __init__ (self ,namestore ,goods) :
-        super().__init__(namestore ,goods)
-        self.adduser = adduser
 x = shop('ร้านอิ่มใจอาหารตามสั่ง','1.กระเพราหมูกรอบ ราคา 20 บาท \n'+ '2.กระเพราะไก่กรอบ ราคา 40 บาท\n'+
- '3.ผัดเผ็ด ราคา 50 บาท\n'+'4.แกงจืด ราคา 60 บาท' ,'s' )
-print(x.adduser)
+ '3.ผัดเผ็ด ราคา 50 บาท\n'+'4.แกงจืด ราคา 60 บาท' )
 x.showfood()
-
 while True:
     menu()
     if choice == 'a':
         shop()
-    elif choice == 'b':
+    '''elif choice == 'b':
         goodsnew()
-    '''elif choice == '3':
+    elif choice == '3':
         delete()
     else:
         yesno2=input('ต้องการออกจากโปรแกรมใช่หรือไม่ y/n : ')
